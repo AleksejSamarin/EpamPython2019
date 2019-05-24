296008be6fe323c57c81d5adb9f64c79a8dcf419
Aleksej <samarin113@gmail.com>
2019-05-24 21:18:23 +0300

    Biology task status: done

--- 01-Data-Structures/carrots/homework_strings.py
+++ 01-Data-Structures/carrots/homework_strings.py
@@ -33,26 +33,73 @@ P.S. За незакрытый файловый дескриптор - кара�
 
 """
 
-# read the file dna.fasta
-dna = None
+import os
+import matplotlib.pyplot as plt
 
 
-def translate_from_dna_to_rna(dna):
-    
-    """your code here"""
-    
+def analyze_line_dna(string: str):
+    if string.startswith('>'):
+        genes.append({'gene': string[1:], 'dna': ''})
+    else:
+        genes[-1]['dna'] += string
+
+
+def translate_from_dna_to_rna(dna: str) -> str:
+    rna = dna.replace('T', 'U')
     return rna
 
 
-def count_nucleotides(dna):
-    
-    """your code here"""
-    
+def count_nucleotides(dna: str) -> dict:
+    num_of_nucleotides = {'A': 0, 'C': 0, 'G': 0, 'T': 0}
+    for nucleotide in num_of_nucleotides.keys():
+        num_of_nucleotides[nucleotide] = dna.count(nucleotide)
     return num_of_nucleotides
 
 
-def translate_rna_to_protein(rna):
-    
-    """your code here"""
-    
+def translate_rna_to_protein(rna: str) -> str:
+    valid_rna = rna if len(rna) % 3 == 0 else rna[:-(len(rna) % 3)]
+    valid_rna_list = [valid_rna[i:i + 3] for i in range(0, len(valid_rna), 3)]
+    protein = ''
+    for triplet in valid_rna_list:
+        protein += protein_replaces[triplet]
     return protein
+
+
+def build_genes_plot():
+    y_pos = tuple(range(len(genes[0]['stats'])))
+    for gene in genes:
+        plt.bar(y_pos, [value / sum(gene['stats'].values()) for value in gene['stats'].values()],
+                label=gene['gene'], alpha=0.5)
+    plt.xticks(y_pos, genes[0]['stats'].keys())
+    plt.title('Histograms for nucleotide gene statistics')
+    plt.xlabel('Nucleotide name')
+    plt.ylabel('Probability density of nucleotide in gene')
+    plt.grid(axis='y')
+    plt.legend()
+    plt.show()
+
+
+if __name__ == '__main__':
+    genes = []
+    path = os.path.dirname(os.path.abspath(__file__))
+
+    with open(os.path.join(path, './files/dna.fasta'), 'r') as file:
+        for line in file:
+            analyze_line_dna(line.rstrip())
+    with open(os.path.join(path, './files/rna_codon_table.txt'), 'r') as file:
+        text = file.read().split()
+    protein_replaces = dict(zip(text[::2], text[1::2]))
+
+    for gene in genes:
+        gene['rna'] = translate_from_dna_to_rna(gene['dna'])
+        gene['stats'] = count_nucleotides(gene['dna'])
+        gene['protein'] = translate_rna_to_protein(gene['rna'])
+
+    to_write = ('stats', 'rna', 'protein')
+    for name in to_write:
+        with open(os.path.join(path, f"./files/output_{name}.txt"), 'w') as file:
+            for gene in genes:
+                file.write(f">{gene['gene']}\n{gene[name]}\n")
+
+    print(genes)
+    build_genes_plot()
