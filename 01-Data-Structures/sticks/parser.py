374fe75d6fe86294d609189f28b8dbbd40781932
Aleksej <samarin113@gmail.com>
2019-05-26 18:07:54 +0300

    Sorted unique wines

--- 01-Data-Structures/sticks/parser.py
+++ 01-Data-Structures/sticks/parser.py
@@ -1,9 +1,27 @@
 import os
 
 
+def list_of_dicts_to_json(dictionary: list) -> str:
+    string_json = str(dictionary)
+    replaces = (("[{'", '[{"', 1), ("'}]", '"}]', 1), ("': '", '": "'),
+                ("', '", '", "'), ("'}, {'", '"}, {"'), ('""', '"'))
+    for replace in replaces:
+        string_json = string_json.replace(*replace)
+    return string_json
+
+
+def unique_dicts(list_of_dicts: list) -> list:
+    return [dict(s) for s in set(tuple(d.items()) for d in list_of_dicts)]
+
+
 def get_data_from_file(name: str) -> str:
-    with open(os.path.join(path, f"./files/{name}"), 'r') as file:
-        return file.read()
+    with open(os.path.join(path, f"./files/{name}"), 'r') as file_r:
+        return file_r.read()
+
+
+def write_to_file(name: str, string: str):
+    with open(os.path.join(path, f"./files/{name}"), 'w') as file_w:
+        file_w.write(string)
 
 
 if __name__ == '__main__':
@@ -15,3 +33,14 @@ if __name__ == '__main__':
     for file_data in raw_data_files:
         for item in file_data:
             json_data_files.append(dict(i.split('": ') for i in item.split(', "')))
+
+    unique_json_data = unique_dicts(json_data_files)
+    sorted_json_data = sorted(unique_json_data, key=lambda i: (i['price'], i['title']), reverse=True)
+    write_to_file('winedata_full.json', list_of_dicts_to_json(sorted_json_data))
+
+    wines = ('Gewürztraminer', 'Riesling', 'Merlot', 'Madera', 'Tempranillo', 'Red Blend')
+    needs = ('average_price', 'min_price', 'max_price',
+             'most_common_region', 'most_common_country', 'average_score')
+    needs_all = ('most_expensive_wine', 'cheapest_wine', 'highest_score',
+                 'lowest_score', 'most_expensive_country', 'cheapest_country',
+                 'most_rated_country', 'underrated_country', 'most_active_commentator')
