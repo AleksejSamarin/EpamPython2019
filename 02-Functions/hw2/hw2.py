bd1d890fd1e5bc1a6f525cc506354863e5071482
Aleksej <samarin113@gmail.com>
2019-05-28 17:28:30 +0300

    hw2 task status: empty delete_value()

---
+++ 02-Functions/hw2/hw2.py
@@ -0,0 +1,29 @@
+def atom(init_value=None):
+    variable = init_value
+
+    def get_value():
+        return variable
+
+    def set_value(set_value=None):
+        nonlocal variable
+        variable = set_value
+        return variable
+
+    def process_value(*functions):
+        nonlocal variable
+        for function in functions:
+            variable = function(variable)
+        return variable
+
+    def delete_value():
+        pass
+
+    local_vars = [*locals().keys()]
+    return get_value, set_value, process_value, delete_value
+
+
+if __name__ == '__main__':
+    func = atom(123)
+    func[1](100)
+    func[2](chr, ord, chr)
+    func[3]()
