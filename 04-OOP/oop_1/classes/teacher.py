628bb55b99b0c4f5fcd18247024cc44de9907921
Aleksej <samarin113@gmail.com>
2019-06-01 19:54:06 +0300

    oop_1 small fix: types added

--- 04-OOP/oop_1/classes/teacher.py
+++ 04-OOP/oop_1/classes/teacher.py
@@ -3,7 +3,7 @@ from .homework import Homework
 
 class Teacher:
 
-    def __init__(self, first_name, last_name):
+    def __init__(self, first_name: str, last_name: str):
         self.first_name = first_name
         self.last_name = last_name
 
