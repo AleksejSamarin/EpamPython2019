c3bdd237356504126d9a6255aa1ddee337f7043a
Aleksej <samarin113@gmail.com>
2019-06-13 15:56:09 +0300

    tests status: preparing

--- 06-advanced-python/tests.py
+++ 06-advanced-python/tests.py
@@ -1,3 +1,12 @@
 """
 Написать тесты(pytest) к предыдущим 3 заданиям, запустив которые, я бы смог бы проверить их корректность
 """
+
+import pytest
+from task1.task1 import PrintableFile, PrintableFolder
+from task2.task2 import Graph
+from task3.task3 import CeasarSipher, ShiftDescriptor
+
+
+if __name__ == '__main__':
+    ...
