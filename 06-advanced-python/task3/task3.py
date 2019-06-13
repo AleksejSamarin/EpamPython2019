4fda2dc8d1d6e356938396306c4c9dacba1fe29e
Aleksej <samarin113@gmail.com>
2019-06-13 13:39:24 +0300

    task3 status: done

--- 06-advanced-python/task3/task3.py
+++ 06-advanced-python/task3/task3.py
@@ -6,11 +6,19 @@
 
 class ShiftDescriptor:
 
+    def __init__(self, shift):
+        self.value = None
+        self.shift = shift
+
+    def modify(self, string):
+        return ''.join(list(map(lambda x: chr(ord(x) + self.shift)
+                            if (ord(x) + self.shift) < 124 else chr(97 + (ord(x) + self.shift) % 123), list(string))))
+
     def __get__(self, instance, owner):
-        pass
+        return self.value
 
     def __set__(self, instance, value):
-        pass
+        self.value = self.modify(value)
 
 
 class CeasarSipher:
@@ -19,9 +27,10 @@ class CeasarSipher:
     another_message = ShiftDescriptor(7)
 
 
-a = CeasarSipher()
-a.message = 'abc'
-a.another_message = 'hello'
+if __name__ == '__main__':
+    a = CeasarSipher()
+    a.message = 'abc'
+    a.another_message = 'hello'
 
-assert a.message == 'efg'
-assert a.another_message == 'olssv'
+    assert a.message == 'efg'
+    assert a.another_message == 'olssv'
