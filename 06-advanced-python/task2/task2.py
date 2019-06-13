d82091d4dd5abc168d632d10a9f66922bd581b43
Aleksej <samarin113@gmail.com>
2019-06-13 12:37:33 +0300

    task2 status: done

--- 06-advanced-python/task2/task2.py
+++ 06-advanced-python/task2/task2.py
@@ -12,9 +12,32 @@ class Graph:
     def __init__(self, E):
         self.E = E
 
+    def __next__(self):
+        if self.is_first:
+            self.is_first = False
+            return self.first_key
+        while len(self.passed) < self.vertices_count:
+            for vertice in self.vertices:
+                if vertice not in self.passed:
+                    self.to_pass.extend(E[vertice])
+                    self.passed.append(vertice)
+                    return vertice
+            self.vertices = self.to_pass[:]
+            self.to_pass.clear()
+        raise StopIteration()
 
-E = {'A': ['B', 'C', 'D'], 'B': ['C'], 'C': [], 'D': ['A']}
-graph = Graph(E)
+    def __iter__(self):
+        self.to_pass = []
+        self.is_first = True
+        self.first_key, self.vertices = tuple(E.items())[0]
+        self.vertices_count = len(E.items())
+        self.passed = [self.first_key]
+        return self
 
-for vertice in graph:
-    print(vertice)
+
+if __name__ == '__main__':
+    E = {'A': ['B', 'C', 'D'], 'B': ['C'], 'C': [], 'D': ['A']}
+    graph = Graph(E)
+
+    for vertice in graph:
+        print(vertice)
