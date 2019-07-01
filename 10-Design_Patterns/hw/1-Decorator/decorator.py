9ec6dd1da7b2986db4063ebedc9088734070a4f8
Aleksej <samarin113@gmail.com>
2019-07-01 22:07:43 +0300

    decorator task status: done

--- 10-Design_Patterns/hw/1-Decorator/decorator.py
+++ 10-Design_Patterns/hw/1-Decorator/decorator.py
@@ -9,14 +9,38 @@ class Component:
         raise NotImplementedError("Override get_cost method")
 
 
-class BaseCoffe(Component):
+class BaseCoffee(Component):
     def get_cost(self):
         return 90
 
 
+class Whip(Component):
+    def __init__(self, product: Component):
+        self.product = product
+
+    def get_cost(self):
+        return self.product.get_cost() + 50
+
+
+class Marshmallow(Component):
+    def __init__(self, product: Component):
+        self.product = product
+
+    def get_cost(self):
+        return self.product.get_cost() + 40
+
+
+class Syrup(Component):
+    def __init__(self, product: Component):
+        self.product = product
+
+    def get_cost(self):
+        return self.product.get_cost() + 30
+
+
 if __name__ == "__main__":
-    coffe = BaseCoffe()
-    coffe = Whip(coffe)
-    coffe = Marshmallow(coffe)
-    coffe = Syrup(coffe)
-    print("Итоговая стоимость за кофе: {}".format(str(coffe.get_cost())))
+    coffee = BaseCoffee()
+    coffee = Whip(coffee)
+    coffee = Marshmallow(coffee)
+    coffee = Syrup(coffee)
+    print("Итоговая стоимость за кофе: {}".format(str(coffee.get_cost())))
